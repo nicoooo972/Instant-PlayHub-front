3fe8acb9a5a114b6cc150229e768acaba72512fd
lebozec <nicolas.lebozec@xelians.fr>
2024-01-16 01:37:17 +0100

    Add sidebar + navbar + cardGame

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
+import React from 'react';
 import './index.css';
+import Sidebar from './components/Sidebar';
+import Navbar from './components/Navbar';
+import Game from './components/Game';
+ // Assurez-vous d'utiliser le chemin correct vers votre fichier Sidebar.tsx
 
 function App() {
+  const gameProps = {
+    id: 'test',
+    name: 'test',
+    type: 'test',
+    version: '1.2',
+  };
+
   return (
     <div>
-      <div className='grid place-items-center h-screen'>Hello world</div>
+      <Navbar />
+      <Sidebar />
+      <Game id={gameProps.id} name={gameProps.name} type={gameProps.type} version={gameProps.version} />
+
     </div>
   );
 }
